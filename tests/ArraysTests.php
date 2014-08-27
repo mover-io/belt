f5c6230baa0f3201a4d1cdc5c9a0c4b5b88df9d6
jacobstr <jacobstr@gmail.com>
2014-08-27 14:13:08 -0600

    Adding a useful function from he php docs.

---
+++ tests/ArraysTests.php
@@ -0,0 +1,57 @@
+<?php namespace PHPUsable;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use Belt\Arrays;
+
+class SchemaObjectTest extends PHPUsableTest
+{
+    public function tests()
+    {
+        PHPUsableTest::$current_test = $this;
+
+        describe('Arrays', function ($test) {
+            describe('Arrays', function ($test) {
+                it('should not convert scalars to arrays', function ($test) {
+                    $a = array(
+                        'level 1' => array(
+                            'level 2' => 2
+                        )
+                    );
+                    $b = array(
+                        'level 1' => array(
+                            'level 2' => 5
+                        )
+                    );
+                    $test->expect(Arrays::array_merge_recursive_distinct($a, $b))->to->eql(
+                        array(
+                            'level 1' => array(
+                                'level 2' => 5
+                            )
+                        )
+                    );
+                });
+
+                it('should unionize arrays', function ($test) {
+                    $a = array(
+                        'level 1' => array(
+                            'level 2' => array(2,3)
+                        )
+                    );
+                    $b = array(
+                        'level 1' => array(
+                            'level 2' => array(5)
+                        )
+                    );
+                    $test->expect(Arrays::array_merge_recursive_distinct($a, $b))->to->eql(
+                        array(
+                            'level 1' => array(
+                                'level 2' => array(5, 3)
+                            )
+                        )
+                    );
+                });
+            });
+        });
+    }
+}
